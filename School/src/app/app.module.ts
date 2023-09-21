9cdce65210f2be7e60b10190f9b262035962db25
adeo7 <bdmosquera7@misena.edu.co>
2023-09-20 22:29:19 -0500

    se agregaron los paquetes t librerias

--- School/src/app/app.module.ts
+++ School/src/app/app.module.ts
@@ -1,16 +1,27 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { CommonModule } from '@angular/common';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ToastrModule } from 'ngx-toastr';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
+    CommonModule,
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    ToastrModule.forRoot({
+      autoDismiss: false,
+      positionClass: "toast-bottom-right"
+    }
+      
+    )
   ],
   providers: [],
   bootstrap: [AppComponent]
